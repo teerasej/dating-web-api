6d1dd576b5af93b4d97a28313ee43a0effd5d107
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-20 18:48:28 +0700

    first commit with mongoose and node

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Teerasej Jiraphatchandej
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+main().catch(error => console.log(error))
+
+async function main() {
+    console.log('Hello World')
+}

bff992e1bc0b15dd0c318d65a9f055099a370e8f
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-20 19:46:55 +0700

    define all routes for restful with user model

--- index.ts
+++ index.ts
@@ -3,10 +3,27 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
 const port = 3000;
+
+app.post('/user', (resquest: Request, response: Response) => {
+
+})
+
+app.get('/user/:id', (resquest: Request, response: Response) => {
+    
+})
+
+app.patch('/user/:id', (resquest: Request, response: Response) => {
+    
+})
+
+app.delete('/user/:id', (resquest: Request, response: Response) => {
+    
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}.`);
